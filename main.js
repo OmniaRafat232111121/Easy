f326bd4d23bcb4cd000efdc9819ff5603fc6d950
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2023-01-16 18:35:40 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -31,6 +31,21 @@ var swiper = new Swiper(".home", {
 
 
 
+const btnn = document.querySelector("button");
+      const post = document.querySelector(".post");
+      const widget = document.querySelector(".star-widget");
+      const editBtn = document.querySelector(".edit");
+      btn.onclick = ()=>{
+        widget.style.display = "none";
+        post.style.display = "block";
+        editBtn.onclick = ()=>{
+          widget.style.display = "block";
+          post.style.display = "none";
+        }
+        return false;
+      }
+
+
 
 
 const setScroll=() => {
